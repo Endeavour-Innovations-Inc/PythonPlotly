eb45b3293b52335f98d2bbfe96c49b80cb2f1695
Endeavour-1 <50341461+Endeavour-Innovations-Inc@users.noreply.github.com>
2023-11-06 16:06:28 -0800

    Update app.py working safe version

--- app.py
+++ app.py
@@ -1,22 +1,60 @@
-from flask import Flask, request, send_from_directory, jsonify
-import os
-from flask_cors import CORS
-import pandas as pd
-from scipy.signal import lfilter
 import dash
 from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+from flask import Flask, send_from_directory, request, send_file
 import plotly.graph_objs as go
+import pandas as pd
+import base64
 import io
+from flask_cors import CORS
+from dash.exceptions import PreventUpdate
+from flask import send_file
+import xlsxwriter
+import csv
+
 
 server = Flask(__name__)
 CORS(server)
+app = dash.Dash(__name__, server=server, routes_pathname_prefix='/dash/')
 
-app = dash.Dash(__name__, server=server, routes_pathname_prefix='/dash/', update_title=None)
-
+# Define initial empty trace and layout for the graph
 initial_trace = go.Scatter(x=[], y=[], mode='lines')
-layout = go.Layout(title="Input Signal", xaxis=dict(title='Time'), yaxis=dict(title='Amplitude (V)'))
+layout = go.Layout(
+    title="Input Signal",
+    xaxis=dict(title='Time'),
+    yaxis=dict(title='Amplitude (V)'),
+    autosize=True
+)
 
+# Define your CSS styles inside a style tag
+styles = {
+    'graphControl': {
+        'display': 'flex',
+        'justifyContent': 'space-between',
+        'alignItems': 'center',
+        'margin': '40px 0',
+        'gap': '15px'
+    },
+    'buttonGroup': {
+        'display': 'flex',
+        'gap': '1px'
+    },
+    'chartDiv': {
+        'borderTop': '1px solid rgba(0, 0, 0, 0.3)',
+        'borderBottom': '1px solid rgba(0, 0, 0, 0.3)',
+        'margin': '20px 0'
+    },
+    'cornerButtons': {
+        'position': 'absolute',
+        'left': '10px',
+        'bottom': '10px',
+        'display': 'flex',
+        'gap': '10px'
+    }
+}
+
+# App layout definition including Upload component and Graph component
+# Define the layout of your app
 app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
@@ -24,62 +62,99 @@ app.layout = html.Div([
         multiple=False
     ),
     html.Button('Reset', id='reset-button'),
-    dcc.Graph(id='my-graph', figure={'data': [initial_trace], 'layout': layout}),
-    html.Button('Export Data', id='export-button')
+    html.Button('Export Data', id='export-button'),
+    
+    # Graph control buttons (styled as per your CSS)
+    html.Div(id='graphControl', style=styles['graphControl'], children=[
+        html.Div(id='buttonGroup', style=styles['buttonGroup'], children=[
+            html.Button('↑', id='zoom-in'),
+            html.Button('↓', id='zoom-out'),
+            html.Button('Sub 3', id='sub-3')
+        ]),
+        html.Button('Button 2', id='btn-2'),
+        html.Button('Button 3', id='btn-3'),
+        html.Button('Button 4', id='btn-4'),
+    ]),
+    
+    # Placeholder for the graph with division lines (styled as per your CSS)
+    html.Div(id='chartDiv', style=styles['chartDiv'], children=[
+        dcc.Graph(id='my-graph', figure={'data': [initial_trace], 'layout': layout}),
+    ]),
+    
+    # Small buttons in the lower left corner (styled as per your CSS)
+    html.Div(id='cornerButtons', style=styles['cornerButtons'], children=[
+        html.Button('Vert', id='vert-button'),
+        html.Button('Horz', id='horz-button'),
+    ]),
+    
+    # Dummy div for triggering downloads
+    html.Div(id='dummy-div')
 ])
 
+# Combined callback for updating graph with uploaded CSV data and resetting the graph
 @app.callback(
     Output('my-graph', 'figure'),
-    [Input('upload-data', 'contents')]
+    [Input('upload-data', 'contents'),
+     Input('reset-button', 'n_clicks')],
+    prevent_initial_call=True
 )
-def update_graph(contents):
-    if contents is None:
+def update_graph(contents, reset_clicks):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        # If no inputs have been triggered, there's nothing to update
+        raise PreventUpdate
+
+    triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if triggered_id == 'upload-data' and contents:
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+
+        trace = go.Scatter(x=df['t'], y=df['data'], mode='lines')
+        return {'data': [trace], 'layout': layout}
+
+    elif triggered_id == 'reset-button':
         return {'data': [initial_trace], 'layout': layout}
 
-    content_string = contents.split(',')[1]
-    decoded = io.StringIO(pd.compat.str(content_string))
-    df = pd.read_csv(decoded)
+    raise PreventUpdate
 
-    trace = go.Scatter(x=df['t'], y=df['data'], mode='lines')
-    return {'data': [trace], 'layout': layout}
-
+# Callback for exporting the current graph's data to Excel
 @app.callback(
-    Output('my-graph', 'figure'),
-    [Input('reset-button', 'n_clicks')]
+    Output('dummy-div', 'children'),
+    [Input('export-button', 'n_clicks')],
+    [State('my-graph', 'figure')],
+    prevent_initial_call=True
 )
-def reset_graph(n):
-    return {'data': [initial_trace], 'layout': layout}
 
-@server.route('/')
-def root():
-    return send_from_directory('.', 'simplescope_wave_viewer.html')
+def export_to_csv(n_clicks, figure_data):
+    if n_clicks is None:
+        raise PreventUpdate
 
-@server.route('/process', methods=['POST'])
-def process_file():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename == '':
-        return 'No file selected', 400
+    global df  # Declare 'df' as global so it can be accessed in the download_csv route
+    df = pd.DataFrame({
+        't': figure_data['data'][0]['x'],
+        'data': figure_data['data'][0]['y']
+    })
 
-    df = pd.read_csv(uploaded_file)
-    data = df['data'].values
+    # Instead of returning send_file, we return a dcc.Location component to redirect.
+    return dcc.Location(href='/download-csv', id='download-csv-trigger')
 
-    n = 5
-    b = [1.0/n] * n
-    a = [1]
-    smoothed_data = lfilter(b, a, data)
 
-    df['smoothed_data'] = smoothed_data
-    response_csv = df.to_csv(index=False)
-    
-    return response_csv
+@server.route('/download-csv')
+def download_csv():
+    # This function uses the 'df' global DataFrame to create a CSV in memory
+    output = io.BytesIO()
+    df.to_csv(output, index=False, encoding='utf-8')
+    output.seek(0)
 
-@server.route('/delete/<filename>', methods=['DELETE'])
-def delete_file(filename):
-    try:
-        os.remove(filename)
-        return jsonify(success=True, message=f"File {filename} deleted successfully.")
-    except FileNotFoundError:
-        return jsonify(success=False, message=f"File {filename} not found."), 404
+    return send_file(
+        output,
+        mimetype='text/csv',
+        as_attachment=True,
+        download_name='data.csv'
+    )
 
 if __name__ == '__main__':
-    server.run(port=8000)
+    app.run_server(debug=True)
