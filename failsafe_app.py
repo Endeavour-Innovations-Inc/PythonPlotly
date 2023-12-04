191d9f843cb52ec8d5481b1538a8917b62481433
Endeavour-1 <50341461+Endeavour-Innovations-Inc@users.noreply.github.com>
2023-12-03 18:36:56 -0800

    Styles outsourced outside the python file
    Much of the styling is gone, this commit is safe from any bugs as of now

--- failsafe_app.py
+++ failsafe_app.py
@@ -9,10 +9,9 @@ import io
 from flask_cors import CORS
 from dash.exceptions import PreventUpdate
 from flask import send_file
-import xlsxwriter
-import csv
 from scipy.signal import lfilter
 from datetime import datetime
+from style import styles, play_icon_style, rectangle_1_style, rectangle_style, triangle_style, thin_rectangle_style, square_icon_style, dropdown_style, combined_rectangle_content_style, combined_rectangle_style, inner_rectangle_style, centered_section_style, long_rectangle_style_scope, long_rectangle_style, row_style, toggle_button_style
 
 
 server = Flask(__name__)
@@ -28,133 +27,179 @@ layout = go.Layout(
     autosize=True
 )
 
-# Define your CSS styles inside a style tag
-styles = {
-    'graphControl': {
-        'display': 'flex',
-        'justifyContent': 'space-between',
-        'alignItems': 'center',
-        'margin': '40px 0',
-        'gap': '15px'
-    },
-    'buttonGroup': {
-        'display': 'flex',
-        'gap': '1px'
-    },
-    'chartDiv': {
-        'borderTop': '1px solid rgba(0, 0, 0, 0.3)',
-        'borderBottom': '1px solid rgba(0, 0, 0, 0.3)',
-        'margin': '20px 0'
-    },
-    'cornerButtons': {
-        'position': 'absolute',
-        'left': '10px',
-        'bottom': '10px',
-        'display': 'flex',
-        'gap': '10px'
-    },
-    'switch': {
-        'display': 'flex',
-        'alignItems': 'center'
-    },
-    'switchInput': {
-        'display': 'none'
-    },
-    'switchLabel': {
-        'position': 'relative',
-        'display': 'inline-block',
-        'width': '60px',
-        'height': '34px'
-    },
-    'switchLabelBefore': {
-        'content': '""',  # This might not work as expected, pseudo-elements may need to be handled differently
-        'position': 'absolute',
-        'top': '0',
-        'left': '0',
-        'width': '100%',
-        'height': '100%',
-        'backgroundColor': '#ccc',
-        'borderRadius': '34px',
-        'transition': '.4s'
-    },
-    'switchLabelAfter': {
-        'content': '""',  # Same issue with content property here
-        'position': 'absolute',
-        'top': '4px',
-        'left': '4px',
-        'width': '26px',
-        'height': '26px',
-        'backgroundColor': 'white',
-        'borderRadius': '50%',
-        'transition': '.4s'
-    },
-    'switchInputChecked': {
-        'backgroundColor': '#2196F3'
-    },
-    'switchLabelAfterChecked': {
-        'transform': 'translateX(26px)'
-    }
-}
+# Style Definitions
 
-# Define a vertical layout style
-vertical_layout_style = {
+rectangle_5_content = html.Div([
+    html.Div("Run", style={'display': 'inline-block', 'verticalAlign': 'middle'}),
+    html.Div(style=play_icon_style)  # This div represents the play icon
+], style={
+    'textAlign': 'center',
     'display': 'flex',
-    'flexDirection': 'column',
-    'alignItems': 'left',
-    'justifyContent': 'center',
-    'gap': '10px',  # Adjust the gap to your liking
-}
+    'alignItems': 'center',  # Center align items vertically
+    'justifyContent': 'center',  # Center align items horizontally
+    'height': '100%',  # Ensure the container takes the full height of the parent
+})
 
-# Define a style for the buttons and upload to make them less wide
-button_style = {
-    'width': 'auto',  # Set the width to 'auto' or specific value to make buttons less wide
-    'maxWidth': '300px',  # Adjust the max width as needed
-    'margin': '0 auto 10px auto'  # Add margins to center the buttons in the parent container
-}
+rectangle_6_content = html.Div([
+    html.Div("Single", style={
+        'display': 'inline-block', 
+        'verticalAlign': 'middle',
+        'marginRight': '10px'  # Add some space between the text and the icon
+    }),
+    html.Div(style={
+        'display': 'inline-block', 
+        'position': 'relative',
+        'verticalAlign': 'middle',
+        'height': '30px',  # Match the height of the triangle and rectangle
+        'width': '40px'  # Adjust the width to contain the triangle and the thin rectangle
+    }, children=[
+        html.Div(style=triangle_style),
+        html.Div(style=thin_rectangle_style)
+    ])
+], style={
+    'textAlign': 'center',
+    'display': 'flex',
+    'alignItems': 'center',
+    'justifyContent': 'center',
+    'height': '100%',
+    'width': '100%'  # Ensure the container takes the full width of the parent
+})
+
+rectangle_7_content = html.Div([
+    html.Div("Stop", style={
+        'display': 'inline-block', 
+        'verticalAlign': 'middle',
+        'marginRight': '10px'  # Space between the text and the icon
+    }),
+    html.Div(style=square_icon_style)  # This div represents the stop icon
+], style={
+    'textAlign': 'center',
+    'display': 'flex',
+    'alignItems': 'center',
+    'justifyContent': 'center',
+    'height': '100%',
+    'width': '100%'
+})
+
+combined_rectangle_content = html.Div([
+    html.Div("Sample Rate", style={'marginBottom': '10px'}),  # Sample Rate text
+    dcc.Dropdown(
+        id='sample-rate-dropdown',
+        options=[
+            {'label': '90 MSPS', 'value': '90MSPS'},
+            {'label': '80 MSPS', 'value': '80MSPS'},
+            {'label': '70 MSPS', 'value': '70MSPS'}
+        ],
+        value='80MSPS',  # Default value
+        clearable=False,
+        style=dropdown_style
+    )
+], style=combined_rectangle_content_style)
+
+# Needs more options for selection
+rectangle_4_content = html.Div([
+    html.Div("Scope Setup", style=inner_rectangle_style),
+    html.Div([
+        html.Div("Coupling", style=centered_section_style),
+        dcc.Dropdown(
+            id='coupling-dropdown',
+            options=[
+                {'label': 'AC', 'value': 'AC'},
+                {'label': 'DC', 'value': 'DC'}
+            ],
+            value='AC',  # Default value
+            clearable=False,
+            style=dropdown_style
+        ),
+        html.Div(id='dummy-output-coupling', style={'display': 'none'}),
+        html.Div("Attenuation", style=centered_section_style),
+        dcc.Dropdown(
+            id='attenuation-dropdown',
+            options=[
+                {'label': '1x', 'value': '1x'},
+                {'label': '10x', 'value': '10x'}
+            ],
+            value='1x',  # Default value
+            clearable=False,
+            style=dropdown_style
+        ),
+        html.Div(id='dummy-output-attenuation', style={'display': 'none'}),
+    ], style={'display': 'inline-block', 'width': '60%', 'textAlign': 'center'})
+], style={'height': '100%', 'width': '100%', 'display': 'flex', 'alignItems': 'center', 'justifyContent': 'space-around'})
+
+# Coupling need to specify more available options
+rectangle_8_content = html.Div([
+    html.Div("Trigger Setup", style=inner_rectangle_style),
+    html.Div([
+        html.Div("Level", style=centered_section_style),
+        dcc.Dropdown(
+            id='level-dropdown',
+            options=[{'label': '1V', 'value': '1V'}, {'label': '5V', 'value': '5V'}],
+            value='1V',  # Default value
+            clearable=False,
+            style=dropdown_style
+        ),
+        html.Div(id='dummy-output-level', style={'display': 'none'}),
+        html.Div("Condition", style=centered_section_style),
+        dcc.Dropdown(
+            id='condition-dropdown',
+            options=[{'label': '↑Rising', 'value': 'Rising'}, {'label': '↓Falling', 'value': 'Falling'}],
+            value='Rising',  # Default value
+            clearable=False,
+            style=dropdown_style
+        ),
+        html.Div(id='dummy-output-condition', style={'display': 'none'}),
+        html.Div("Force Trigger", style=centered_section_style),
+        html.Button("Off", id='force-trigger-button', style=toggle_button_style),
+        html.Div(id='dummy-output-force-trigger', style={'display': 'none'}),
+    ], style={'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center'})
+], style={'height': '100%', 'width': '100%', 'display': 'flex', 'alignItems': 'center', 'justifyContent': 'space-around'})
+
+@app.callback(
+    Output('force-trigger-button', 'children'),
+    [Input('force-trigger-button', 'n_clicks')],
+    prevent_initial_call=True
+)
+def toggle_force_trigger(n_clicks):
+    if n_clicks % 2 == 0:
+        return "Off"
+    else:
+        return "On"
+
+# First row of rectangles with adjusted layout
+first_row_rectangles = html.Div(style=row_style, children=[
+    html.Div(id='rectangle-1', children="Connect to Simple Scope", style=rectangle_1_style),
+    html.Div(id='boolean-value', children='True', style={'display': 'none'}),
+    html.Div(style={'width': '10px', 'display': 'inline-block'}),  # Spacer Div
+    html.Div(id='combined-rectangle-2-3', children=combined_rectangle_content, style=combined_rectangle_style),
+    html.Div(id='dummy-output-sample-rate', style={'display': 'none'}),
+    html.Div(id='rectangle-4', children=rectangle_4_content, style=long_rectangle_style_scope),
+])
 
 # App layout definition including Upload component and Graph component
 # Define the layout of your app
 app.layout = html.Div([
-    html.Div([
-        dcc.Upload(
-            id='upload-data',
-            children=html.Button('Upload File', style={'width': '10%'}),
-            multiple=False,
-            style={'width': '100%'}
-        ),
-        html.Button('Enable Filter: Off', id='filter-toggle', n_clicks=0, style={'width': '10%'}),
-        html.Button('Reset', id='reset-button', style={'width': '10%'}),
-        html.Button('Export Data', id='export-button', style={'width': '10%'}),
-    ], style=vertical_layout_style),
-    
-    # Graph control buttons (styled as per your CSS)
-    # Graph control buttons (styled as per your CSS)
-    html.Div(id='graphControl', style=styles['graphControl'], children=[
-        html.Div(id='buttonGroup1', style=styles['buttonGroup'], children=[
-            html.Button('↑', id='zoom-in1'),
-            html.Button('Sub 3', id='sub-31'),
-            html.Button('↓', id='zoom-out1'),
-        ]),
-        html.Div(id='buttonGroup2', style=styles['buttonGroup'], children=[
-            html.Button('↑', id='zoom-in2'),
-            html.Button('↓', id='zoom-out2'),
-            html.Button('Sub 3', id='sub-32')
-        ]),
-        html.Button('Button 3', id='btn-3'),
-        html.Button('Button 4', id='btn-4'),
-    ]),
 
+    # Place new elements here
+    # First row of rectangles
+    first_row_rectangles,
+
+    # Second row of rectangles
+    html.Div(style=row_style, children=[
+        html.Div(id='rectangle-6', children=rectangle_6_content, style=rectangle_style),
+        html.Div(id='single-boolean-value', children='True', style={'display': 'none'}),
+        html.Div(style={'width': '10px', 'display': 'inline-block'}),  # Spacer Div
+        html.Div(id='rectangle-5', children=rectangle_5_content, style=rectangle_style),
+        html.Div(id='run-boolean-value', children='True', style={'display': 'none'}),
+        html.Div(id='rectangle-7', children=rectangle_7_content, style=rectangle_style),
+        html.Div(id='stop-boolean-value', children='True', style={'display': 'none'}),
+        html.Div(id='rectangle-8', children=rectangle_8_content, style=long_rectangle_style),  # Another longer rectangle
+    ]),
     
     # Placeholder for the graph with division lines (styled as per your CSS)
     html.Div(id='chartDiv', style=styles['chartDiv'], children=[
         dcc.Graph(id='my-graph', figure={'data': [initial_trace], 'layout': layout}),
     ]),
-    
-    # Small buttons in the lower left corner (styled as per your CSS)
-    # html.Div(id='cornerButtons', style=styles['cornerButtons'], children=[
-    #    html.Button('Vert', id='vert-button'),
-    #    html.Button('Horz', id='horz-button'),
-    # ]),
 
     # Add a large transparent rectangle with a smaller rectangle and a button inside
     html.Div(id='large-rectangle', style={
@@ -190,7 +235,9 @@ app.layout = html.Div([
     html.Div(id='upload-timestamp', style={'display': 'none'}),
     
     # Component for triggering downloads
-    dcc.Download(id='download-dataframe-csv')
+    dcc.Download(id='download-dataframe-csv'),
+
+    dcc.Store(id='tracked-data-store', storage_type='memory', data=["Off", "Rising", "1V", "1x", "AC", "80MSPS"])
 ])
 
 # Initialize 'df' as a global variable outside of your callbacks
@@ -281,7 +328,129 @@ def export_data(n_clicks):
     # Trigger download
     return dcc.send_data_frame(df.to_csv, "data.csv", index=False)
 
-# Callback to toggle the "Normal" button label
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# New UI callbacks for Dash
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Coupling
+@app.callback(
+    Output('dummy-output-coupling', 'children'),
+    [Input('coupling-dropdown', 'value')],
+    prevent_initial_call=True
+)
+def log_coupling_change(new_value):
+    print(f"Coupling changed to {new_value}")
+    return ""  # Dummy output, not used
+
+# Attentuation
+@app.callback(
+    Output('dummy-output-attenuation', 'children'),
+    [Input('attenuation-dropdown', 'value')],
+    prevent_initial_call=True
+)
+def log_attenuation_change(new_value):
+    print(f"Attenuation changed to {new_value}")
+    return ""  # Dummy output, not used
+
+# Sample Rate
+@app.callback(
+    Output('dummy-output-sample-rate', 'children'),
+    [Input('sample-rate-dropdown', 'value')],
+    prevent_initial_call=True
+)
+def log_sample_rate_change(new_value):
+    print(f"Sample rate changed to {new_value}")
+    return ""  # Dummy output, not used
+
+# Single Button
+@app.callback(
+    Output('single-boolean-value', 'children'),
+    [Input('rectangle-6', 'n_clicks')],
+    [State('single-boolean-value', 'children')]
+)
+def toggle_single_boolean_value(n_clicks, current_value):
+    if n_clicks is None:
+        raise PreventUpdate
+    
+    new_value = 'False' if current_value == 'True' else 'True'
+    print(f"Single button boolean value changed to {new_value}")  # Log the change
+    return new_value
+
+# Run Button
+@app.callback(
+    Output('run-boolean-value', 'children'),
+    [Input('rectangle-5', 'n_clicks')],
+    [State('run-boolean-value', 'children')]
+)
+def toggle_run_boolean_value(n_clicks, current_value):
+    if n_clicks is None:
+        raise PreventUpdate
+    
+    new_value = 'False' if current_value == 'True' else 'True'
+    print(f"Run button boolean value changed to {new_value}")  # Log the change
+    return new_value
+
+# Stop Button
+@app.callback(
+    Output('stop-boolean-value', 'children'),
+    [Input('rectangle-7', 'n_clicks')],
+    [State('stop-boolean-value', 'children')]
+)
+def toggle_stop_boolean_value(n_clicks, current_value):
+    if n_clicks is None:
+        raise PreventUpdate
+    
+    new_value = 'False' if current_value == 'True' else 'True'
+    print(f"Stop button boolean value changed to {new_value}")  # Log the change
+    return new_value
+
+# Level Selection:
+@app.callback(
+    Output('dummy-output-level', 'children'),
+    [Input('level-dropdown', 'value')],
+    prevent_initial_call=True
+)
+def log_level_change(new_value):
+    print(f"Level changed to {new_value}")
+    return ""  # Dummy output, not used
+
+# Condition DropDown
+@app.callback(
+    Output('dummy-output-condition', 'children'),
+    [Input('condition-dropdown', 'value')],
+    prevent_initial_call=True
+)
+def log_condition_change(new_value):
+    print(f"Condition changed to {new_value}")
+    return ""  # Dummy output, not used
+
+# Force trigger
+@app.callback(
+    Output('dummy-output-force-trigger', 'children'),
+    [Input('force-trigger-button', 'n_clicks')],
+    [State('dummy-output-force-trigger', 'children')],
+    prevent_initial_call=True
+)
+def toggle_force_trigger_button(n_clicks, current_state):
+    new_state = 'On' if current_state == 'Off' else 'Off'
+    print(f"Force Trigger state changed to {new_state}")  # Log the change
+    return new_state
+
+# Connect to simple scope variable record
+@app.callback(
+    Output('boolean-value', 'children'),
+    [Input('rectangle-1', 'n_clicks')],
+    [State('boolean-value', 'children')]
+)
+def toggle_boolean_value(n_clicks, current_value):
+    if n_clicks is None:
+        raise PreventUpdate
+    
+    new_value = 'False' if current_value == 'True' else 'True'
+    print(f"Boolean value changed to {new_value}")  # Log the change
+    return new_value
+
+# Confirmed tracking of the variable
 @app.callback(
     Output('normal-button', 'children'),
     [Input('normal-button', 'n_clicks')],
@@ -289,10 +458,9 @@ def export_data(n_clicks):
     prevent_initial_call=True
 )
 def toggle_normal_button(n_clicks, current_label):
-    if current_label == 'Normal':
-        return 'FFT'
-    else:
-        return 'Normal'
+    new_label = 'FFT' if current_label == 'Normal' else 'Normal'
+    print(f"Normal button label changed to {new_label}")  # Log the change
+    return new_label
 
 if __name__ == '__main__':
     app.run_server(debug=True)
